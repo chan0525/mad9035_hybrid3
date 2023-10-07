e0c93c7668e4a3cd17970c3886b3fe86a376c129
Bruno Chan <chan0525@algonquinlive.com>
2023-10-06 22:54:37 -0400

    create String variable containing a JSON string

--- bin/dart_collections.dart
+++ bin/dart_collections.dart
@@ -1,5 +1,18 @@
+import 'dart:io';
+import 'dart:convert';
 import 'package:dart_collections/dart_collections.dart' as dart_collections;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_collections.calculate()}!');
+void main() {
+
+  String json = '''
+  [
+    {"first":"Steve", "last":"Griffith", "email":"griffis@algonquincollege.com"},
+    {"first":"Adesh", "last":"Shah", "email":"shaha@algonquincollege.com"},
+    {"first":"Tony", "last":"Davidson", "email":"davidst@algonquincollege.com"},
+    {"first":"Adam", "last":"Robillard", "email":"robilla@algonquincollege.com"}
+  ]
+  ''';
+
+ 
+
 }
